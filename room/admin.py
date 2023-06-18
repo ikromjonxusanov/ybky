35d65b17c6085ce5b9617b4431bd0ad3dcef8c00
ikromjonxusanov <ikromjonkhusanov06@gmail.com>
2023-06-18 16:52:48 +0500

    added BookAdmin

--- room/admin.py
+++ room/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from room.models import Room
+from room.models import Room, Book
 
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'type', 'capacity',)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'room', 'resident', 'start', 'end')
