e68351eb07d5c4a9627d005e8d9575b11611f99d
ikromjonxusanov <ikromjonkhusanov06@gmail.com>
2023-06-19 22:51:04 +0500

    added cors headers

--- config/settings.py
+++ config/settings.py
@@ -20,18 +20,22 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'room',
     'rest_framework',
-    'django_filters'
+    'django_filters',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'config.urls'
 
